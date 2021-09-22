4a6c885ed64a8647146ff232d103f9ff40353663
Aymeric <aymeric.bertrand35@gmail.com>
2021-09-22 10:43:54 +0200

    Create TheHeader.vue

---
+++ components/TheHeader.vue
@@ -0,0 +1,8 @@
+<template>
+    <header class="py-6 lg:text-3xl fixed w-screen top-0 bg-indigo-900">
+        <ul class="flex gap-8 lg:gap-32 justify-center">
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+    </header>
+</template>
