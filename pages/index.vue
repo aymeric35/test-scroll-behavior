e1e905bdd96a6146aabca5810177e10ca81be3bc
Aymeric <aymeric.bertrand35@gmail.com>
2021-09-22 10:44:17 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div class="container h-screen flex items-center justify-center">
+      <h1 class="text-center lg:text-2xl">Hello, this is the home page.</h1>
+    </div>
+    <div class="container h-screen flex items-center justify-center">
+      <p class="text-center lg:text-2xl">placeholder</p>
+    </div>
+    <div class="container h-screen flex items-center justify-center">
+      <p class="text-center lg:text-2xl">placeholder</p>
+    </div>
+    <div class="container h-screen flex items-center justify-center">
+      <p class="text-center lg:text-2xl">placeholder</p>
+    </div>
+    <div class="container h-screen flex items-center justify-center">
+      <p class="text-center lg:text-2xl">placeholder</p>
+    </div>
+    <div class="container h-screen flex items-center justify-center">
+      <p id="contact" class="text-center lg:text-2xl">
+        This is the contact anchor
+      </p>
+    </div>
+  </div>
 </template>
-
-<script>
-export default {}
-</script>
