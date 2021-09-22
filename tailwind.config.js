b7b2c10f35b8907a5860b3e0f9afa08e7de30d5e
Aymeric <aymeric.bertrand35@gmail.com>
2021-09-22 10:44:21 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
